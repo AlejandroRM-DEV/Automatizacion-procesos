01c30370c570bbb1d6d0a1ebd41e1d3fd49d3f0a
Alejandro Ramírez Muñoz <alejandror45@gmail.com>
2023-02-14 16:39:58 -0600

    Progress bar and concurrent

--- src/video-sec.py
+++ src/video-sec.py
@@ -1,12 +1,12 @@
 import os
 import cv2
+import glob
+import concurrent.futures
+from tqdm import tqdm
 
 
 def generate_image_sequence(video_path, image_interval):
     cap = cv2.VideoCapture(video_path)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     image_counter = 0
@@ -27,27 +27,28 @@ def generate_image_sequence(video_path, image_interval):
     cap.release()
 
 
-def list_files_by_extension(extension):
+def process_video(video_file, image_interval):
+    generate_image_sequence(video_file, image_interval)
+
+
+def list_files():
     video_files = []
-    current_dir = os.getcwd()
-    for root, dirs, files in os.walk(current_dir):
-        for file in files:
-            if file.endswith(extension):
-                video_files.append(os.path.join(root, file))
+    current_dir = "."
+    for extension in [".mp4", ".avi", ".mov", ".dav"]:
+        video_files.extend(glob.glob(f"{current_dir}/**/*{extension}", recursive=True))
 
     return video_files
 
 
-extension = input("Introduce la extensión de los archivos (ejemplo: '.mp4'): ")
-video_files = list_files_by_extension(extension)
+video_files = list_files()
 
 if len(video_files) == 0:
-    print("No se encontraron archivos de video con esa extensión")
+    print("No se encontraron archivos de video")
 else:
     image_interval = float(input("Introduce cada cuántos segundos quieres una imagen: "))
-    for video_file in video_files:
-        print(f"Procesando video: {video_file}")
-        generate_image_sequence(video_file, image_interval)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(process_video, video_file, image_interval) for video_file in video_files]
+        for _ in tqdm(concurrent.futures.as_completed(results), total=len(results), desc="Procesando videos"):
+            pass
     
     print("Fin")
-
